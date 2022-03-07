1f1c1e2d4b4143feb22538bc8af7592f8d105ac3
moominot <47282347+moominot@users.noreply.github.com>
2022-03-07 19:25:35 +0100

    Add files via upload

---
+++ coaib.js
@@ -0,0 +1,8 @@
+//https://drive.google.com/uc?export=download&id=1RIz8E2wmKGWdPSWaA31Va83xwQf2sb_Y
+
+setTimeout(function(){
+    document.querySelector('mat-drawer-content').style.marginLeft = 0;
+    document.querySelector('mat-drawer').style.position = "relative";
+        document.querySelector('mat-drawer').style.width = "auto";
+        },500)
+
