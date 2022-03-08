6ab872a5286f14cd631e022373f22262fa3c2f63
moominot <47282347+moominot@users.noreply.github.com>
2022-03-08 12:19:34 +0100

    Update coaib2.js

--- coaib2.js
+++ coaib2.js
@@ -1,6 +1,13 @@
-location.replace("https://e.coaib.org/"); 
-setTimeout(function(){
-        document.querySelector('mat-drawer').style.position = "relative";
+var button = document.createElement("Button");
+button.innerHTML = "recarrega";
+button.style = "top:0;right:0;position:absolute;z-index: 9999"
+butoon.onclick = magia;
+document.body.appendChild(button);
+
+function magia(){
+document.querySelector('mat-drawer').style.position = "relative";
         document.querySelector('mat-drawer').style.width = "100%";
+setTimeout(function(){
         document.querySelector('mat-drawer-content').style.marginLeft = "0px";
     },10000);
+}
