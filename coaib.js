a9bb46bc5facdd2d2b6b65870664d76cdba49855
moominot <47282347+moominot@users.noreply.github.com>
2022-03-08 10:44:08 +0100

    Update coaib.js

--- coaib.js
+++ coaib.js
@@ -1,8 +1,5 @@
-//https://drive.google.com/uc?export=download&id=1RIz8E2wmKGWdPSWaA31Va83xwQf2sb_Y
-
-
-    document.querySelector('mat-drawer-content').style.marginLeft = "0px";
+  
     document.querySelector('mat-drawer').style.position = "relative";
-        document.querySelector('mat-drawer').style.width = "100%";
-        
+    document.querySelector('mat-drawer').style.width = "100%";
+    document.querySelector('mat-drawer-content').style.marginLeft = "0px";    
 
