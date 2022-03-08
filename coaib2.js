cc88ff8d8be62cc276bbe6237d5410e7f388e29b
moominot <47282347+moominot@users.noreply.github.com>
2022-03-08 12:05:59 +0100

    Create coaib2.js

---
+++ coaib2.js
@@ -0,0 +1,8 @@
+location.replace("https://e.coaib.org/"); 
+
+
+    setTimeout(function(){
+      document.querySelector('mat-drawer').style.position = "relative";
+    document.querySelector('mat-drawer').style.width = "100%";
+      document.querySelector('mat-drawer-content').style.marginLeft = "0px";
+    },5000);
