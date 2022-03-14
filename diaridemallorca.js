b4750b4cce3f332bf5c88c9f90d74624474ef0b7
moominot <47282347+moominot@users.noreply.github.com>
2022-03-14 23:20:48 +0100

    Create diaridemallorca.js

---
+++ diaridemallorca.js
@@ -0,0 +1,3 @@
+document.querySelectorAll(".article-body--truncated").forEach(r => r.classList.remove("article-body--truncated"))
+
+document.querySelector(".article-body--seo-closed").style.display = "initial";
