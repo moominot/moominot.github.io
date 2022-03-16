3c1bd9bd2fee2c6b81bc6d6d420b06eb8cc8f8b5
moominot <47282347+moominot@users.noreply.github.com>
2022-03-16 01:21:41 +0100

    Update diaridemallorca.js

--- diaridemallorca.js
+++ diaridemallorca.js
@@ -1,4 +1,4 @@
 document.querySelectorAll(".article-body--truncated").forEach(r => r.classList.remove("article-body--truncated"))
 
 document.querySelector(".article-body--seo-closed").style.display = "initial";
-document.getElementById("checkout-container").remove();
+document.getElementById("paywall").remove();
