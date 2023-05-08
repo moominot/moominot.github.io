174ddffee442830e488e6ad7efc31e6c9c40270d
moominot <47282347+moominot@users.noreply.github.com>
2023-05-08 12:27:13 +0200

    Update js.js

--- js.js
+++ js.js
@@ -25,9 +25,9 @@ const
 //for (const value of values) console.log(value);
 // shirt, blue, , m
 
-//for(const entry of entries) {
-  //console.log(`${entry[0]}: ${entry[1]}`);
-//}
+for(const entry of entries) {
+  console.log(`${entry[0]}: ${entry[1]}`);
+}
 // product: shirt
 // color: blue
 // newuser:
