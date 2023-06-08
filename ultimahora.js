5c269f283a8866abc1a9228293a9d6fab28dd3d9
moominot <47282347+moominot@users.noreply.github.com>
2023-06-08 12:53:57 +0200

    Update ultimahora.js

--- ultimahora.js
+++ ultimahora.js
@@ -1,3 +1,4 @@
 document.querySelector(".unregistered-content").remove()
 document.querySelector(".registered-content").classList.remove("registered-content")
 document.querySelector(".teads-inread").remove()
+document.querySelector(".henneoHB-intext").remove()
