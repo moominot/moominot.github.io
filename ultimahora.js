8578ab938e1e3469c3744f32217df79534267f20
moominot <47282347+moominot@users.noreply.github.com>
2023-06-08 12:51:59 +0200

    Update ultimahora.js

--- ultimahora.js
+++ ultimahora.js
@@ -1,2 +1,3 @@
 document.querySelector(".unregistered-content").remove()
 document.querySelector(".registered-content").classList.remove("registered-content")
+document.querySelector(".teads-inread").remove()
