a68fbd4116a1e967f69dbc10cdff328e91d696b6
moominot <47282347+moominot@users.noreply.github.com>
2023-08-01 12:55:34 +0200

    Create elpais.js

---
+++ elpais.js
@@ -0,0 +1,7 @@
+document.querySelector("#ctn_freemium_article").remove()
+
+var artamagat = document.querySelector(".a_b_wall._dn")
+  artamagat.classList.add("a_c")
+  artamagat.classList.remove("a_b_wall")
+  artamagat.classList.remove("_dn")
+
