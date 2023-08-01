cfd784a492b4dc231bf23621e56d56b7ccfb924e
moominot <47282347+moominot@users.noreply.github.com>
2023-08-01 13:38:09 +0200

    Update arabalears.js

--- arabalears.js
+++ arabalears.js
@@ -1,6 +1,10 @@
 document.querySelector(".paywall").remove()
 document.body.classList.remove("closed")
-var amagat = document.querySelector(".ara-body div")
-  amagat.style.display="block"
-  amagat.classList.add("ara-body")
+var amagat = document.querySelectorAll(".ara-body div")
+amagat.forEach(div=>{
+  if(div.style.display="none"){
+  div.style.display="block"  
+  div.classList.add("ara-body")}
+})
+  
 
