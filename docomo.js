adbc40cbdcceabd7810725ebaadd11932b033b2d
moominot <47282347+moominot@users.noreply.github.com>
2024-09-25 17:15:26 +0200

    Update docomo.js

--- docomo.js
+++ docomo.js
@@ -1,9 +1,16 @@
+const collapse = document.getElementById("collapseGaleria")
+collapse.classList.add("show")
+
+let element = document.querySelectorAll(".galeria-item")
+let darrerElement = element[element.length-1]
+darrerElement.scrollIntoView({ behavior: 'smooth' });
+
 const modals = document.querySelectorAll(".do-not-open");
 
 modals.forEach((m) => {
   var link = document.createElement("a");
   link.innerHTML = m.innerHTML;
-  link.href = link.querySelector("img").src.replace("-350x245","");
+  link.href = m.querySelector("img").src.replace("-350x245","");
   m.parentNode.appendChild(link);
 });
 
